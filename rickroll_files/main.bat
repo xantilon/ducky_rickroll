796bd29cd76764fba7d62fe631c9105ad7fc343e
Edgar <xantilon@gmail.com>
2022-11-29 21:29:05 +0100

    Update main.bat

--- rickroll_files/main.bat
+++ rickroll_files/main.bat
@@ -1,5 +1,5 @@
-powershell Invoke-WebRequest -Uri https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -OutFile yt-dlp.exe
+powershell -NoProfile Invoke-WebRequest -Uri https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe -OutFile yt-dlp.exe
 yt-dlp.exe -o rickroll.mp4 https://www.youtube.com/watch?v=dQw4w9WgXcQ
 start rickroll.mp4
-timeout /t 0.2
+timeout /t 1
 powershell $wshell = New-Object -ComObject wscript.shell;$wshell.SendKeys('{F11}')
